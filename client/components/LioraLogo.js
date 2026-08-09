2064d6582ffc312d56385e75504d397ff907544a
Jahidul Islam Jisan <jisan22205101743@diu.edu.bd>
2026-08-09 18:21:30 +0600

    Enlarge LIORA logo to 72px and fix search bar 100 percent width centering

--- client/components/LioraLogo.js
+++ client/components/LioraLogo.js
@@ -7,7 +7,7 @@ export default function LioraLogo({ className = "", style = {} }) {
       alt="LIORA Beauty & Wear Logo"
       className={`jt-liora-logo-img ${className}`}
       style={{
-        height: "62px",
+        height: "72px",
         width: "auto",
         objectFit: "contain",
         display: "block",
